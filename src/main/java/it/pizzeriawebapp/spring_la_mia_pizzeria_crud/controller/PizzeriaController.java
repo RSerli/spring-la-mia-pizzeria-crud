521f61495b6ecf891d793411a863abcd7d821754
Rserli <riccardoserli.95@gmail.com>
2025-09-24 21:58:00 +0200

    Added info button for all items

--- src/main/java/it/pizzeriawebapp/spring_la_mia_pizzeria_crud/controller/PizzeriaController.java
+++ src/main/java/it/pizzeriawebapp/spring_la_mia_pizzeria_crud/controller/PizzeriaController.java
@@ -1,11 +1,13 @@
 package it.pizzeriawebapp.spring_la_mia_pizzeria_crud.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.pizzeriawebapp.spring_la_mia_pizzeria_crud.model.Pizza;
@@ -29,5 +31,18 @@ public class PizzeriaController {
         List<Pizza> result = repository.findAll();
         model.addAttribute("listaPizze", result);
         return "index";
+    }
+
+    @GetMapping ("/PizzeriaSpring/Detail/{id}")
+    public String index(@PathVariable("id") Integer id, Model model){
+        Optional<Pizza> optionalPizza = repository.findById(id);
+        if(optionalPizza.isPresent()){
+            model.addAttribute("selectedPizza", optionalPizza.get());
+            model.addAttribute("isSelectedPizza", true);
+        }else{
+            model.addAttribute("isSelectedPizza", false);
+
+        }
+        return "showDetail";
     }
 }
