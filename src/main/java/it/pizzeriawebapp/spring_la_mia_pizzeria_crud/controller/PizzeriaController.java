4d9daa0b4429cf871772b55a890c586213e00c41
Rserli <riccardoserli.95@gmail.com>
2025-09-27 19:53:34 +0200

    post method added

--- src/main/java/it/pizzeriawebapp/spring_la_mia_pizzeria_crud/controller/PizzeriaController.java
+++ src/main/java/it/pizzeriawebapp/spring_la_mia_pizzeria_crud/controller/PizzeriaController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import it.pizzeriawebapp.spring_la_mia_pizzeria_crud.model.Pizza;
 import it.pizzeriawebapp.spring_la_mia_pizzeria_crud.repository.PizzeriaRepository;
@@ -66,14 +68,22 @@ public class PizzeriaController {
     }
     
     @PostMapping("/PizzeriaSpring/NewPizza")
-    public String save(@Valid @ModelAttribute("newPizza") Pizza userInput, BindingResult bindingResult) {
+    public String save(@Valid @ModelAttribute("newPizza") Pizza userInput, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         
+        // controllo se non viene inserito un nuovo elemento con lo stesso nome
+        Optional<Pizza> optionalPizza = repository.findByNome(userInput.getNome());
+        if (optionalPizza.isPresent())
+        {
+            bindingResult.addError(new ObjectError("errorDuplictedName", "Pizza con lo stesso nome già presente!"));
+            model.addAttribute("warningAlertMessage", " Pizza con lo stesso nome già presente!");
+        }
+
         if(bindingResult.hasErrors()){
-            return "/PizzeriaSpring/NewPizza";
+            return "addNewPizzaForm";
         }
 
         repository.save(userInput);
-        
+        redirectAttributes.addFlashAttribute("successAlertMessage", "Nuova pizza inerita correttamente!");
         return "redirect:/PizzeriaSpring/index";
     }
     
--- src/main/java/it/pizzeriawebapp/spring_la_mia_pizzeria_crud/repository/PizzeriaRepository.java
+++ src/main/java/it/pizzeriawebapp/spring_la_mia_pizzeria_crud/repository/PizzeriaRepository.java
@@ -1,6 +1,7 @@
 package it.pizzeriawebapp.spring_la_mia_pizzeria_crud.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,7 +9,9 @@ import it.pizzeriawebapp.spring_la_mia_pizzeria_crud.model.Pizza;
 
 public interface PizzeriaRepository extends JpaRepository<Pizza, Integer>{
 
-    // query  Custom sul nome della pizza
+    // query  Custom sul nome della pizza anche con non nome completo
     public List<Pizza> findByNomeContainingIgnoreCase(String nome);
 
+    // query  Custom sul nome COMPLETO della pizza
+    public Optional<Pizza> findByNome(String nome);
 }
